5452d6c1f7f762e7a6562502d3476d4f28193a59
wanghao1993 <whao53333@gmail.com>
2024-05-12 23:25:41 +0800

    feat: bugreport

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,7 +1,10 @@
+import { Button } from "antd";
 export function Header() {
   return (
     <>
-      <div className="layout-header h-[48px]">Header</div>
+      <div className="layout-header h-[48px]">
+        <Button>按钮</Button>
+      </div>
     </>
   );
 }
